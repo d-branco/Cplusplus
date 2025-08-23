81a7aeb4093d4ae5d8e22ff621e7e8608f1811e2
gris <185072671+d-branco@users.noreply.github.com>
2025-08-23 18:36:19 +0100

    formatter

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abessa-m <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 16:00:05 by abessa-m          #+#    #+#             */
-/*   Updated: 2025/08/23 16:00:12 by abessa-m         ###   ########.fr       */
+/*   Updated: 2025/08/23 18:55:12 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main(void)
+int main(void)
 {
 	std::cout << "Hello world!\n";
+	return (0);
 }
