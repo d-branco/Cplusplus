d671550567ee8650ba4622cb34b03865a49049c7
gris <185072671+d-branco@users.noreply.github.com>
2025-08-23 21:10:14 +0100

    no bonus

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,14 +6,15 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 16:00:05 by abessa-m          #+#    #+#             */
-/*   Updated: 2025/08/23 18:55:12 by abessa-m         ###   ########.fr       */
+/*   Updated: 2025/08/23 21:33:44 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cstdlib>
 #include <iostream>
 
 int main(void)
 {
 	std::cout << "Hello world!\n";
-	return (0);
+	return (EXIT_SUCCESS);
 }
