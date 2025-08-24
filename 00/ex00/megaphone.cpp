f8b9c10b6ba0de5fd371e318f774bfe842e72332
gris <185072671+d-branco@users.noreply.github.com>
2025-08-24 18:14:49 +0100

    standard character out

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,15 +6,31 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 16:00:05 by abessa-m          #+#    #+#             */
-/*   Updated: 2025/08/23 21:33:44 by abessa-m         ###   ########.fr       */
+/*   Updated: 2025/08/24 18:24:25 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cstdlib>
 #include <iostream>
 
-int main(void)
-{
-	std::cout << "Hello world!\n";
-	return (EXIT_SUCCESS);
+int main(int argc, char **argv) {
+  int i;
+  int j;
+
+  if (argc == 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+    return (EXIT_SUCCESS);
+  }
+
+  i = 1;
+  while (i < argc) {
+    j = 0;
+    while (argv[i][j]) {
+      std::cout << (char)std::toupper(argv[i][j]);
+      j++;
+    }
+    i++;
+  }
+  std::cout << '\n';
+  return (EXIT_SUCCESS);
 }
