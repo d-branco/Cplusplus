8c163e163c7d31b13cd88b415a80bcaff50250ab
gris <185072671+d-branco@users.noreply.github.com>
2025-08-25 08:00:14 +0100

    tidy

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,31 +6,33 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 16:00:05 by abessa-m          #+#    #+#             */
-/*   Updated: 2025/08/24 18:24:25 by abessa-m         ###   ########.fr       */
+/*   Updated: 2025/08/25 07:53:58 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cstdlib>
 #include <iostream>
 
-int main(int argc, char **argv) {
-  int i;
-  int j;
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (EXIT_SUCCESS);
+	}
 
-  if (argc == 1) {
-    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-    return (EXIT_SUCCESS);
-  }
-
-  i = 1;
-  while (i < argc) {
-    j = 0;
-    while (argv[i][j]) {
-      std::cout << (char)std::toupper(argv[i][j]);
-      j++;
-    }
-    i++;
-  }
-  std::cout << '\n';
-  return (EXIT_SUCCESS);
+	int chr;
+	int arg = 1;
+	while (arg < argc)
+	{
+		chr = 0;
+		while (argv[arg][chr] != '\0')
+		{
+			std::cout << (char) std::toupper(argv[arg][chr]);
+			chr++;
+		}
+		arg++;
+	}
+	std::cout << '\n';
+	return (EXIT_SUCCESS);
 }
