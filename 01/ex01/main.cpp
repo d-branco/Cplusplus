8302c3bb55ee8daf1939c7fcc1f2b554431cf9c5
gris <185072671+d-branco@users.noreply.github.com>
2025-08-31 16:10:01 +0100

    NULL, zero and valgrind

--- 01/ex01/main.cpp
+++ 01/ex01/main.cpp
@@ -5,7 +5,7 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/08/31 15:43:31      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/08/31 15:49:00     #########  #########  ###      ###      */
+/*   Updated: 2025/08/31 16:09:11     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
@@ -16,11 +16,27 @@ Zombie *zombieHorde(int N, std::string name);
 int		main()
 {
 	Zombie *ptr1 = zombieHorde(2, "Alice");
+	for (int i = 0; i < 2; i++)
+	{
+		ptr1[i].announce();
+	}
 	Zombie *ptr2 = zombieHorde(2, "Bob");
+	for (int i = 0; i < 2; i++)
+	{
+		ptr2[i].announce();
+	}
 	Zombie *ptr3 = zombieHorde(5, "Charlie");
-	// ptr = zombieHorde(0, "Delta");
-	// delete(ptr);
+	for (int i = 0; i < 5; i++)
+	{
+		ptr3[i].announce();
+	}
+	Zombie *ptr4 = zombieHorde(0, "Delta");
+	for (int i = 0; i < 0; i++)
+	{
+		ptr4[i].announce();
+	}
 	delete[] ptr1;
 	delete[] ptr2;
 	delete[] ptr3;
+	delete[] ptr4;
 }
