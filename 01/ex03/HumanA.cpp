3a79b222690357dc8fec642eac28507de6bb2491
gris <185072671+d-branco@users.noreply.github.com>
2025-08-31 18:42:19 +0100

    HumanA

--- 01/ex03/HumanA.cpp
+++ 01/ex03/HumanA.cpp
@@ -4,7 +4,29 @@
 /*                                             +:+         :+:   :+:          */
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
-/*   Created: 2025/08/31 16:35:58      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/08/31 16:38:22     #########  #########  ###      ###      */
+/*   Created: 2025/08/31 18:27:08      #+#         #+#      +#+        #+#    */
+/*   Updated: 2025/08/31 18:35:39     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
+
+#include "HumanA.hpp"
+
+HumanA::HumanA(std::string name, Weapon &weapon) :
+	name(name),
+	weapon_type(weapon)
+{
+}
+
+void HumanA::setWeapon(Weapon new_weapon)
+{
+	weapon_type = new_weapon;
+}
+
+Weapon HumanA::get_weapon()
+{
+	return (weapon_type);
+}
+
+void HumanA::attack()
+{
+}
--- 01/ex03/HumanA.hpp
+++ 01/ex03/HumanA.hpp
@@ -4,8 +4,8 @@
 /*                                             +:+         :+:   :+:          */
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
-/*   Created: 2025/08/31 18:13:34      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/08/31 18:14:29     #########  #########  ###      ###      */
+/*   Created: 2025/08/31 18:34:36      #+#         #+#      +#+        #+#    */
+/*   Updated: 2025/08/31 18:35:16     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@
 class HumanA
 {
   private:
-	Weapon		weapon_type;
 	std::string name;
+	Weapon		weapon_type;
 
   public:
-	HumanA(std::string name, Weapon weapon_a);
+	HumanA(std::string name, Weapon &weapon_type);
 
-	void   setWeapon(Weapon weapon_a);
+	void   setWeapon(Weapon new_weapon);
 	Weapon get_weapon();
 
 	void   attack();
