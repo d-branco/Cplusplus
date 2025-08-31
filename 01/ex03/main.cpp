cefe1562b9f3fba6bf3ca65b916a860321fb24ed
gris <185072671+d-branco@users.noreply.github.com>
2025-08-31 17:11:58 +0100

    Unnecessary violence

---
+++ 01/ex03/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                          ::::::::    ::::::::   :::::::::  */
+/*   main.cpp                             :+:    :+:  :+:    :+:  :+:         */
+/*                                             +:+         :+:   :+:          */
+/*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
+/*                                       +#+         +#+              +#+     */
+/*   Created: 2025/08/31 16:35:58      #+#         #+#      +#+        #+#    */
+/*   Updated: 2025/08/31 17:11:06     #########  #########  ###      ###      */
+/*                                                            ########        */
+/* ************************************************************************** */
+
+int main()
+{
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return 0;
+}
