37bafd97c7442b1be681e32c846e89fcda18ad5e
gris <185072671+d-branco@users.noreply.github.com>
2025-09-08 17:21:14 +0100

    02/ex02 init

---
+++ 02/ex02/Fixed.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                          ::::::::    ::::::::   :::::::::  */
+/*   Fixed.hpp                            :+:    :+:  :+:    :+:  :+:         */
+/*                                             +:+         :+:   :+:          */
+/*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
+/*                                       +#+         +#+              +#+     */
+/*   Created: 2025/09/02 17:45:10      #+#         #+#      +#+        #+#    */
+/*   Updated: 2025/09/02 19:41:29     #########  #########  ###      ###      */
+/*                                                            ########        */
+/* ************************************************************************** */
+
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <cmath>
+#include <iostream>
+#include <string>
+
+//	Default		constructor
+//
+//	Copy		constructor
+//	Copy		assignment operator
+//
+//	Destructor
+
+class Fixed
+{
+  private:
+	int				 value_;
+	static const int fractional_bits_ = 8; // always equal to 8
+
+  public:
+	Fixed();
+
+	Fixed(const Fixed &other);
+	Fixed &operator=(const Fixed &other);
+
+	~Fixed();
+
+	int	 getRawBits() const;
+	void setRawBits(int raw);
+
+	Fixed(const int int_nbr);
+	Fixed(const float float_nbr);
+
+	float toFloat() const;
+	int	  toInt() const;
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
+
+#endif
---
+++ 02/ex02/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                          ::::::::    ::::::::   :::::::::  */
+/*   main.cpp                             :+:    :+:  :+:    :+:  :+:         */
+/*                                             +:+         :+:   :+:          */
+/*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
+/*                                       +#+         +#+              +#+     */
+/*   Created: 2025/09/02 16:36:43      #+#         #+#      +#+        #+#    */
+/*   Updated: 2025/09/02 16:37:59     #########  #########  ###      ###      */
+/*                                                            ########        */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+#include <string>
+
+int main()
+{
+	Fixed		a;
+	Fixed const b(10);
+	Fixed const c(42.42f);
+	Fixed const d(b);
+
+	a = Fixed(1234.4321f);
+
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+	return (0);
+}
