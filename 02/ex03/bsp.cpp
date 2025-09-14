0e236a37bb4784bce2a811913fb67c5ca3a5ec61
cluster <185072671+d-branco@users.noreply.github.com>
2025-09-14 12:56:07 +0100

    check if points in line

--- 02/ex03/bsp.cpp
+++ 02/ex03/bsp.cpp
@@ -13,6 +13,30 @@
 
 #include "Point.hpp"
 
+Fixed		distance_to_line(Point line0, Point line1, Point point);
+
 bool		bsp(Point const a, Point const b, Point const c, Point const point)
 {
+	//check if there are any three points aligned
+	if ((distance_to_line(a, b, c) == 0) || (distance_to_line(a, b, point) == 0)
+		|| (distance_to_line(b, c, a) == 0) || (distance_to_line(b, c, point) == 0)
+		|| (distance_to_line(c, a, b) == 0) || (distance_to_line(c, a, point) == 0))
+		return (false);
+
+	return (true);
+}
+
+Fixed		distance_to_line(Point l_start, Point l_end, Point point)
+{
+	Fixed a = l_end.get_y() - l_start.get_y();
+	Fixed b = l_start.get_x() - l_end.get_x();
+	Fixed c = (l_end.get_x() * l_start.get_y())
+					- (l_start.get_x() * l_end.get_y());
+
+	Fixed num = (a * point.get_x()) + (b * point.get_y()) + c;
+	if ((num == 0) || ((a == 0) && (b == 0)))
+		return (0);
+	else if (num < 0)
+		num = (num * -1);
+	return (num);
 }
--- 02/ex03/main.cpp
+++ 02/ex03/main.cpp
@@ -11,10 +11,26 @@
 
 #include "Fixed.hpp"
 #include "Point.hpp"
+#include <iostream>
 
 bool bsp(Point const a, Point const b, Point const c, Point const point);
 
 int	 main(void)
 {
-	return 0;
+	Point a(0,0);
+	Point b(4,2);
+	Point c(2,4);
+
+	Point p(1,1);
+
+	if (bsp(a, b, c, p) == true)
+	{
+		std::cout << "Funciona!\n";
+	}
+	else
+	{
+		std::cout << "Nao funciona correctamente!\n";
+	}
+
+	return (0);
 }
