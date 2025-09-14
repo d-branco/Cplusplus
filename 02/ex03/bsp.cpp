e3020f316cac6f97c902ab29c52dbfb03116548c
cluster <185072671+d-branco@users.noreply.github.com>
2025-09-14 13:02:56 +0100

    check line side

--- 02/ex03/bsp.cpp
+++ 02/ex03/bsp.cpp
@@ -14,6 +14,7 @@
 #include "Point.hpp"
 
 Fixed		distance_to_line(Point line0, Point line1, Point point);
+Fixed		line_side(Point l_start, Point l_end, Point point);
 
 bool		bsp(Point const a, Point const b, Point const c, Point const point)
 {
@@ -23,8 +24,22 @@ bool		bsp(Point const a, Point const b, Point const c, Point const point)
 		|| (distance_to_line(c, a, b) == 0) || (distance_to_line(c, a, point) == 0))
 		return (false);
 
-	return (true);
+	//check if point is on the same side as the remaining vertice
+	if ((line_side(a, b, c) == line_side(a, b, point))
+		&& (line_side(b, c, a) == line_side(b, c, point))
+		&& (line_side(c, a, b) == line_side(c, a, point)))
+		return (true);
+	return (false);
 }
+
+Fixed		line_side(Point l_start, Point l_end, Point point)
+{
+	if (((l_end.get_x() - l_start.get_x()) * (point.get_y() - l_start.get_y())
+			- (l_end.get_y() - l_start.get_y()) * (point.get_x() - l_start.get_x())) > 0)
+		return (1);
+	return (-1);
+}
+
 
 Fixed		distance_to_line(Point l_start, Point l_end, Point point)
 {
