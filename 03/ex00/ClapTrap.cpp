872a7cd23e53442cb038659d25457b793ffaf1bb
gris <185072671+d-branco@users.noreply.github.com>
2025-09-15 15:09:17 +0100

    tickles

--- 03/ex00/ClapTrap.cpp
+++ 03/ex00/ClapTrap.cpp
@@ -4,8 +4,8 @@
 /*                                             +:+         :+:   :+:          */
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
-/*   Created: 2025/09/15 11:18:03      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/09/15 11:25:23     #########  #########  ###      ###      */
+/*   Created: 2025/09/15 15:12:35      #+#         #+#      +#+        #+#    */
+/*   Updated: 2025/09/15 15:18:05     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ ClapTrap::ClapTrap(const std::string &name) :
 	energy_(10),
 	attack_(0)
 {
+	std::cout << this->name_ << " online.\n";
 }
 
 ClapTrap::ClapTrap(const ClapTrap &other) :
@@ -25,6 +26,7 @@ ClapTrap::ClapTrap(const ClapTrap &other) :
 	energy_(other.energy_),
 	attack_(other.attack_)
 {
+	std::cout << this->name_ << " online.\n";
 }
 
 ClapTrap &ClapTrap::operator=(const ClapTrap &other)
@@ -34,11 +36,14 @@ ClapTrap &ClapTrap::operator=(const ClapTrap &other)
 	this->energy_ = other.energy_;
 	this->attack_ = other.attack_;
 
+	std::cout << this->name_ << " online.\n";
+
 	return (*this);
 }
 
 ClapTrap::~ClapTrap()
 {
+	std::cout << this->name_ << " offline.\n";
 }
 
 std::string ClapTrap::get_name()
@@ -62,15 +67,34 @@ void ClapTrap::increment_hit(int change)
 
 void ClapTrap::attack(const std::string &target)
 {
-	(void) target;
+	if ((this->hit_ == 0) || (this->energy_ == 0))
+	{
+		std::cout << this->name_ << " stays still.\n";
+		return;
+	}
+	std::cout << this->name_ << " attacks " << target << " dealing "
+			  << this->attack_ << " damage!\n";
+	this->energy_--;
 }
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
-	(void) amount;
+	if (this->hit_ == 0)
+	{
+		std::cout << this->name_ << " remains immovable.\n";
+	}
+	std::cout << this->name_ << " takes " << amount << "damage!\n";
+	ClapTrap::increment_hit(-amount);
 }
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
-	(void) amount;
+	if ((this->hit_ == 0) || (this->energy_ == 0))
+	{
+		std::cout << this->name_ << " stays still.\n";
+	}
+	this->energy_--;
+	ClapTrap::increment_hit(amount);
+	std::cout << this->name_ << " recovers " << amount << " hit points. Has "
+			  << this->hit_ << " total.\n";
 }
--- 03/ex00/main.cpp
+++ 03/ex00/main.cpp
@@ -4,8 +4,8 @@
 /*                                             +:+         :+:   :+:          */
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
-/*   Created: 2025/09/15 11:12:25      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/09/15 11:17:21     #########  #########  ###      ###      */
+/*   Created: 2025/09/15 15:08:55      #+#         #+#      +#+        #+#    */
+/*   Updated: 2025/09/15 15:10:59     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
@@ -19,13 +19,16 @@ int main()
 	ClapTrap tagarela("balabolka");
 	ClapTrap treta("bladdermouth");
 
-	std::cout << "Names: " << tagarela.get_name() << " and " << treta.get_name()
-			  << "\n";
+	std::cout << "\nNames: " << tagarela.get_name() << " and "
+			  << treta.get_name() << "\n\n";
 
-	ClapTrap terceiro(treta);
+	tagarela.attack("bladdermouth");
 
-	std::cout << "Names: " << terceiro.get_name() << " and " << treta.get_name()
-			  << "\n";
+	std::cout << "\n";
+
+	treta.beRepaired(1);
+
+	std::cout << "\n";
 
 	return (EXIT_SUCCESS);
 }
