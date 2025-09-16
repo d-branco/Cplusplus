f2b816ce0700f548b0177b3a17c9ad9f340d7393
gris <185072671+d-branco@users.noreply.github.com>
2025-09-16 12:52:40 +0100

    Dog in form

--- 04/ex00/Dog.cpp
+++ 04/ex00/Dog.cpp
@@ -5,8 +5,32 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/09/16 11:51:40      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/09/16 11:54:18     #########  #########  ###      ###      */
+/*   Updated: 2025/09/16 12:53:13     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
+
+///////////////////////////////////////////////////// Canonical Orthodox Form //
+Dog::Dog(std::string type) : Animal(type)
+{
+}
+
+Dog::Dog(const Dog &other) : Animal(other.type_)
+{
+	*this = other;
+}
+
+Dog &Dog::operator=(const Dog &other)
+{
+	if (this != &other)
+	{
+		this->type_ = other.type_;
+		std::cout << "It's similar to the previous " << type_ << ".\n";
+	}
+	return (*this);
+}
+
+Dog::~Dog()
+{
+}
