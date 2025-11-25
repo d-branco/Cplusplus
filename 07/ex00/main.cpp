7423952a4df7a433bf85f5f8c81805801dae6f74
charlie <185072671+d-branco@users.noreply.github.com>
2025-11-25 14:39:39 +0000

    mod07ex00 finit

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -5,7 +5,7 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/11/25 14:17:09      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/11/25 14:32:46     #########  #########  ###      ###      */
+/*   Updated: 2025/11/25 14:35:19     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@ int main(void)
 	int a = 2;
 	int b = 3;
 
-	// ::swap(a, b);
-	// std::cout << "a = " << a << ", b = " << b << std::endl;
-	// std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
 	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 
-	// ::swap(c, d);
-	// std::cout << "c = " << c << ", d = " << d << std::endl;
-	// std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
 	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
 
 	return (EXIT_SUCCESS);
--- 07/ex00/template.hpp
+++ 07/ex00/template.hpp
@@ -5,12 +5,11 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/11/25 14:32:00      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/11/25 14:33:57     #########  #########  ###      ###      */
+/*   Updated: 2025/11/25 14:39:10     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
 template <typename T>
-
 T max(T x, T y)
 {
 	if (x > y)
@@ -18,4 +17,23 @@ T max(T x, T y)
 		return (x);
 	}
 	return (y);
+}
+
+template <typename T>
+T min(T x, T y)
+{
+	if (x < y)
+	{
+		return (x);
+	}
+	return (y);
+}
+
+template <typename T>
+void swap(T &x, T &y)
+{
+	T temp = x;
+	x = y;
+	y = temp;
+	return ;
 }
