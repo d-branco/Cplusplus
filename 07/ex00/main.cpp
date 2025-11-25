db1d16fb3a834c8a4bbcaa8cfba1c1f67970f614
charlie <185072671+d-branco@users.noreply.github.com>
2025-11-25 14:34:13 +0000

    max template

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -5,28 +5,31 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/11/25 14:17:09      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/11/25 14:21:22     #########  #########  ###      ###      */
+/*   Updated: 2025/11/25 14:32:46     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
 #include <cstdlib>
+#include <iostream>
+
+#include "template.hpp"
 
 int main(void)
 {
 	int a = 2;
 	int b = 3;
 
-	::swap(a, b);
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	// ::swap(a, b);
+	// std::cout << "a = " << a << ", b = " << b << std::endl;
+	// std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
 	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	// ::swap(c, d);
+	// std::cout << "c = " << c << ", d = " << d << std::endl;
+	// std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
 	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
 
 	return (EXIT_SUCCESS);
