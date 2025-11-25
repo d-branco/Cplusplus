0f3f39baab58e1d6ae25f5f8b45e778d4a6749ae
charlie <185072671+d-branco@users.noreply.github.com>
2025-11-25 16:56:48 +0000

    Extra tests

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -5,26 +5,42 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/11/25 14:42:58      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/11/25 16:36:22     #########  #########  ###      ###      */
+/*   Updated: 2025/11/25 16:56:22     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 #define ITER_HPP
 
-template <typename T, typename U>
-void iter(T *array, const size_t lenght, U function)
+template <typename T>
+void iter(T *array, const size_t lenght, void (*function)(T &))
 {
 	size_t i = 0;
 	for (;;)
 	{
-		array[i] = function(array[i]);
+		function(array[i]);
 		i++;
 		if (i >= lenght)
-		{
 			break;
-		}
 	}
+}
+
+template <typename T>
+void iter(const T *array, const size_t lenght, void (*function)(const T &))
+{
+	size_t i = 0;
+	for (;;)
+	{
+		function(array[i]);
+		i++;
+		if (i >= lenght)
+			break;
+	}
+}
+
+template <typename T> void add_one(T &ori)
+{
+	ori++;
 }
 
 #endif
--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -5,7 +5,7 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/11/25 14:42:58      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/11/25 16:36:22     #########  #########  ###      ###      */
+/*   Updated: 2025/11/25 16:57:12     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
@@ -14,21 +14,22 @@
 
 #include "iter.hpp"
 
-int add_one(int ori);
-
 int main(void)
 {
 	int array[3] = {1, 2, 3};
-
 	std::cout << "Original array: " << array[0] << ", " << array[1] << ", "
 			  << array[2] << "\n";
+
 	iter(array, 3, add_one);
 	std::cout << "Final array:    " << array[0] << ", " << array[1] << ", "
 			  << array[2] << "\n";
-	return (EXIT_SUCCESS);
-}
 
-int add_one(int ori)
-{
-	return (ori + 1);
+	char arr[5] = {'C', 'E', 'S', 'A', 'R'};
+	std::cout << "Original array: " << arr[0] << ", " << arr[1] << ", "
+			  << arr[2] << ", " << arr[3] << ", " << arr[4] << "\n";
+
+	iter(arr, 3, add_one);
+	std::cout << "Final array:    " << arr[0] << ", " << arr[1] << ", "
+			  << arr[2] << ", " << arr[3] << ", " << arr[4] << "\n";
+	return (EXIT_SUCCESS);
 }
