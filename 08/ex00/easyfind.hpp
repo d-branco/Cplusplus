72d13cf5d7348c2a2234efd53f61dfcbe7591506
charlie <185072671+d-branco@users.noreply.github.com>
2025-11-26 17:38:28 +0000

    ::iterator

--- 08/ex00/easyfind.hpp
+++ 08/ex00/easyfind.hpp
@@ -5,7 +5,7 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/11/26 15:46:09      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/11/26 16:08:09     #########  #########  ###      ###      */
+/*   Updated: 2025/11/26 17:48:44     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
@@ -16,59 +16,42 @@
 # define DEBUG false
 #endif
 
+#include <algorithm>
 #include <iostream>
 
-template <class T> class Easyfind
+template <typename T>
+typename T::const_iterator easyfind(const T &int_container, int nbr)
 {
-  private:
-
-  public:
-	Easyfind()
+	if (DEBUG)
 	{
-		if (DEBUG)
+		std::cout << "==DEBUG== Easyfind()\n";
+	}
+	typename T::const_iterator pos = int_container.begin();
+	typename T::const_iterator end = int_container.end();
+	try
+	{
+		for (;;)
 		{
-			std::cout << "==DEBUG== Easyfind: Default Parametized Constructor\n";
+			if (*pos == nbr)
+			{
+				std::cout << "INT " << *pos << " found\n";
+				return (pos);
+			}
+
+			if (pos == end)
+			{
+				throw std::exception();
+				std::cout << "TESTE\n ";
+			}
+			pos++;
 		}
 	}
-
-	Easyfind(const Easyfind &other)
+	catch (const std::exception &e)
 	{
-		if (DEBUG)
-		{
-			std::cout << "==DEBUG== Easyfind: Copy Constructor\n";
-		}
-		*this = other;
+		std::cout << "Error: INT " << nbr << " not found\n";
 	}
 
-	Easyfind &operator=(const Easyfind &other)
-	{
-		if (DEBUG)
-		{
-			std::cout << "==DEBUG== Easyfind: Copy Operator Constructor\n";
-		}
-		if (this == &other)
-		{
-			return (*this);
-		}
-
-		return (*this);
-	}
-
-	~Easyfind()
-	{
-		if (DEBUG)
-		{
-			std::cout << "==DEBUG== Easyfind: Destructor\n";
-		}
-	}
-
-	find(type T, int nbr)
-	{
-		if (DEBUG)
-		{
-			std::cout << "==DEBUG== Easyfind: find\n";
-		}
-	}
+	return (end);
 };
 
 #endif
--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -5,13 +5,37 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/11/26 15:46:09      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/11/26 15:53:51     #########  #########  ###      ###      */
+/*   Updated: 2025/11/26 17:45:55     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
+#include <algorithm>
 #include <cstdlib>
+#include <vector>
+
+#include "easyfind.hpp"
 
 int main(void)
 {
+	std::vector<int> array;
+	std::cout << "array:\n";
+	int i = 1;
+	for (;;)
+	{
+		array.push_back(i);
+		std::cout << i;
+		i *= 2;
+		if (i > 225)
+		{
+			std::cout << "\n";
+			break;
+		}
+		std::cout << ", ";
+	}
+	std::cout << "\nSearching for int: 16\n";
+	std::vector<int>::const_iterator pos = easyfind(array, 16);
+	std::cout << "\nSearching for int: 42\n";
+	pos = easyfind(array, 42);
+
 	return (EXIT_SUCCESS);
 }
