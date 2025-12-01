13625ea6812c972fb316bf57c3519e77d562408a
charlie <185072671+d-branco@users.noreply.github.com>
2025-12-01 14:38:40 +0000

    dual file access validation

--- 09/ex00/main.cpp
+++ 09/ex00/main.cpp
@@ -5,7 +5,7 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/11/30 15:48:40      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/11/30 17:29:53     #########  #########  ###      ###      */
+/*   Updated: 2025/12/01 14:30:27     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
@@ -21,43 +21,81 @@
 # define dprint(msg) ((void) 0)
 #endif
 
-#define INPUT_FILE argv[1]
+#define DB_FILE "../cpp_09/data.csv"
 
+int			input_validation(int			argc,
+							 char		  **argv,
+							 std::ifstream &database_file,
+							 std::ifstream &input_file);
 std::string get_extension(const char *file_name);
 
 int			main(int argc, char **argv)
 {
 	dprint("Debug mode activated");
+	std::ifstream db_file(DB_FILE);
+	std::ifstream i_file(argv[1]);
+	if (input_validation(argc, argv, i_file, db_file) != 0)
+	{
+		return (EXIT_FAILURE);
+	}
+
+	dprint("");
+	dprint("End of main()");
+	return (EXIT_SUCCESS);
+}
+
+int input_validation(int			argc,
+					 char		  **argv,
+					 std::ifstream &database_file,
+					 std::ifstream &input_file)
+{
+	dprint("");
+	dprint("input_validation(): Checking number of arguments");
 	if (argc == 1)
 	{
-		std::cout << "Error: Missing database \nUsage: \n  ./btc input.csv\n";
+		std::cout << "Error: Missing input \nUsage: \n  ./btc input.txt\n";
 		return (EXIT_FAILURE);
 	}
 	if (argc > 2)
 	{
-		std::cout << "Error: Too many arguments \nUsage: \n  ./btc input.csv\n";
+		std::cout << "Error: Too many arguments \nUsage: \n  ./btc input.txt\n";
 		return (EXIT_FAILURE);
 	}
-	dprint("Input file: " << INPUT_FILE);
+	dprint("input_validation(): Input file: " << argv[1]);
 
-	dprint("Checking if file exists and is readable");
-	std::ifstream file(INPUT_FILE);
-	if (!file.good())
+	dprint("input_validation(): Input file: Checking if the input file exists "
+		   "and is "
+		   "readable");
+	if (!input_file.good())
 	{
-		std::cout << "Error: Could nor access file " << INPUT_FILE << "\n";
+		std::cout << "Error: Could not access file " << argv[1] << "\n";
 		return (EXIT_FAILURE);
-	}
-
-	if (get_extension(INPUT_FILE) == ".csv")
-	{
-		dprint(INPUT_FILE << " is good");
 	}
 	else
 	{
-		dprint("Extension: " << get_extension(INPUT_FILE));
+		dprint("input_validation(): Input file: " << argv[1] << " is good");
+	}
+
+	dprint("input_validation(): DB file: " << DB_FILE);
+	dprint("input_validation(): DB file: Checking if the database exists and "
+		   "is readable");
+	if (!database_file.good())
+	{
+		std::cout << "Error: Could not access database " << DB_FILE << "\n";
+		return (EXIT_FAILURE);
+	}
+	if (get_extension(DB_FILE) == ".csv")
+	{
+		dprint("input_validation(): DB file: " << DB_FILE << " is good");
+	}
+	else
+	{
+		dprint("input_validation(): DB file: Extension: "
+			   << get_extension(DB_FILE));
 		std::cout << "Error: File does not have the \'.csv\' extention \n";
 		return (EXIT_FAILURE);
 	}
+	dprint("");
 
 	return (EXIT_SUCCESS);
 }
