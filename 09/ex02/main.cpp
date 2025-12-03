81522c438ebfeb00b6ad25eefd2b8233e7f41c42
delta <185072671+d-branco@users.noreply.github.com>
2025-12-03 16:59:16 +0000

    int.length()

--- 09/ex02/main.cpp
+++ 09/ex02/main.cpp
@@ -5,7 +5,7 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/12/02 20:26:16      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/12/03 13:12:22     #########  #########  ###      ###      */
+/*   Updated: 2025/12/03 16:58:34     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 # define dprint(msg) ((void) 0)
 #endif
 
+int get_nbr_length(int nbr);
+
 // stack: adapts a container to provide stack (LIFO data structure)
 int main(int argc, char **argv)
 {
@@ -29,16 +31,44 @@ int main(int argc, char **argv)
 		std::cerr << "Error\n";
 		return (EXIT_FAILURE);
 	}
-	else
+	int maximum = atol(argv[1]);
+	int minimum = atol(argv[1]);
+	for (int i = 1; argv[i] != 0; ++i)
 	{
-		for (int i = 1; argv[i] != 0; ++i)
+		dprint("argv[" << i << "]: " << argv[i]);
+		if (maximum < atol(argv[i]))
 		{
-			dprint("argv[" << i << "]: " << argv[i]);
+			maximum = atol(argv[i]);
+		}
+		else if (minimum > atol(argv[i]))
+		{
+			minimum = atol(argv[i]);
 		}
 	}
+	dprint("Maximum element: " << maximum
+							   << " length: " << get_nbr_length(maximum));
+	dprint("Minimum element: " << minimum
+							   << " length: " << get_nbr_length(minimum));
+
 	dprint("");
 
 	dprint("");
 	dprint("End of main()");
 	return (EXIT_SUCCESS);
+}
+
+int get_nbr_length(int nbr)
+{
+	int len = 0;
+	if (nbr < 0)
+	{
+		len++;
+		nbr = -nbr;
+	}
+	while (nbr > 0)
+	{
+		nbr /= 10;
+		len++;
+	}
+	return (len);
 }
