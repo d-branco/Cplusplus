5d8ea1c9b0c398ad49891450162d1e273af1d33e
gris <185072671+d-branco@users.noreply.github.com>
2025-12-03 13:13:01 +0000

    light formatting

--- 09/ex02/main.cpp
+++ 09/ex02/main.cpp
@@ -5,7 +5,7 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/12/02 20:26:16      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/12/03 12:28:36     #########  #########  ###      ###      */
+/*   Updated: 2025/12/03 13:12:22     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
@@ -19,12 +19,11 @@
 # define dprint(msg) ((void) 0)
 #endif
 
-
 // stack: adapts a container to provide stack (LIFO data structure)
-int			main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	dprint("Debug mode activated");
-	if (argc <2)
+	if (argc < 2)
 	{
 		dprint("No argument prodived");
 		std::cerr << "Error\n";
@@ -32,12 +31,13 @@ int			main(int argc, char **argv)
 	}
 	else
 	{
-		for (int i = 1; argv[i]!=0; ++i)
-			{dprint("argv[" << i << "]: " << argv[i]);}
+		for (int i = 1; argv[i] != 0; ++i)
+		{
+			dprint("argv[" << i << "]: " << argv[i]);
+		}
 	}
 	dprint("");
 
-	
 	dprint("");
 	dprint("End of main()");
 	return (EXIT_SUCCESS);
