0f1435ff0dc4ec16be77e45aa9c4777d29a10f47
gris <185072671+d-branco@users.noreply.github.com>
2025-12-03 11:03:45 +0000

    mod09ex01 finit

--- 09/ex01/main.cpp
+++ 09/ex01/main.cpp
@@ -5,7 +5,7 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/12/02 20:26:16      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/12/02 22:27:30     #########  #########  ###      ###      */
+/*   Updated: 2025/12/03 11:12:19     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 # define dprint(msg) ((void) 0)
 #endif
 
-std::string stack_to_string(std::stack<char> polish);
+std::string stack_to_string(std::stack<int> polish);
 
 // stack: adapts a container to provide stack (LIFO data structure)
 int			main(int argc, char **argv)
@@ -35,20 +35,62 @@ int			main(int argc, char **argv)
 	}
 	dprint("");
 
-	std::stack<char> polish;
+	std::stack<int> polish;
 
-	size_t			 ite = 0;
-	// int				 nbr;
+	size_t			ite = 0;
+	int				nbr;
 	while (argv[1][ite])
 	{
-		dprint("argv[1][" << ite << "]: " << argv[1][ite]);
-		if (((argv[1][ite] >= '0') && (argv[1][ite] <= '9'))
-			|| (argv[1][ite] == '+') || (argv[1][ite] == '-')
-			|| (argv[1][ite] == '/') || (argv[1][ite] == '*'))
+		dprint("Reading input: " << argv[1][ite]);
+		if ((argv[1][ite] >= '0') && (argv[1][ite] <= '9'))
 		{
-			polish.push(argv[1][ite]);
+			polish.push(argv[1][ite] - '0');
+			dprint("Stack: [" << stack_to_string(polish) << " ]");
 			// dprint("nbr: " << nbbr);
 			// nbr = atoi(argv[ite]);
+		}
+		else if ((argv[1][ite] == '+') || (argv[1][ite] == '-')
+				 || (argv[1][ite] == '/') || (argv[1][ite] == '*'))
+		{
+			nbr = (polish.top());
+			polish.pop();
+			// dprint("nbr = " << nbr);
+			// dprint("Stack: [" << stack_to_string(polish) << " ]");
+			switch (argv[1][ite])
+			{
+			case '+':
+			{
+				nbr += (polish.top());
+				break;
+			}
+			case '-':
+			{
+				nbr = (polish.top()) - nbr;
+				break;
+			}
+			case '/':
+			{
+				nbr = (polish.top()) / nbr;
+				break;
+			}
+			case '*':
+			{
+				nbr = (polish.top()) * nbr;
+				break;
+			}
+			default:
+			{
+				dprint("Unreachable \'swich case default\'");
+			}
+			}
+
+			polish.pop();
+			dprint("Stack: [" << stack_to_string(polish) << " ]");
+			dprint("nbr = " << nbr);
+			polish.push(nbr);
+			dprint("Stack: [" << stack_to_string(polish) << " ]");
+			dprint("Result: " << polish.top());
+			dprint("");
 		}
 		else
 		{
@@ -56,6 +98,11 @@ int			main(int argc, char **argv)
 			return (EXIT_FAILURE);
 		}
 		++ite;
+		if (argv[1][ite] != ' ')
+		{
+			std::cerr << "Error\n";
+			return (EXIT_FAILURE);
+		}
 		if (argv[1][ite] != 0)
 		{
 			++ite;
@@ -64,12 +111,13 @@ int			main(int argc, char **argv)
 
 	dprint("Stack: [" << stack_to_string(polish) << " ]");
 
+	std::cout << polish.top() << "\n";
 	dprint("");
 	dprint("End of main()");
 	return (EXIT_SUCCESS);
 }
 
-std::string stack_to_string(std::stack<char> polish)
+std::string stack_to_string(std::stack<int> polish)
 {
 	std::ostringstream oss;
 	while (!polish.empty())
