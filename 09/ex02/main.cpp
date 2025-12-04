11007c951449a69415dd9488b788c02fe9aa2964
gris <185072671+d-branco@users.noreply.github.com>
2025-12-04 13:01:54 +0000

    slip into functions

--- 09/ex02/main.cpp
+++ 09/ex02/main.cpp
@@ -5,7 +5,7 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/12/02 20:26:16      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/12/03 21:27:49     #########  #########  ###      ###      */
+/*   Updated: 2025/12/04 13:05:55     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
@@ -21,76 +21,119 @@
 # define dprint(msg) ((void) 0)
 #endif
 
-int get_nbr_length(int nbr);
+typedef struct s_init t_init;
 
-// std::array
-// std::vector
-// std::list
-// std::deque
-// std::string -> since its a container, it is technically a valid option
+int					  get_nbr_length(int nbr);
+void				  get_duration(t_init &s_i);
+
+typedef struct s_init
+{
+	int			   maximum_value;
+	int			   array_size;
+	int			   nbr_length;
+	struct timeval start_time;
+	struct timeval end_time;
+	long		   duration;
+} t_init;
+
+int initializer(int argc, char **argv, t_init &s_i);
+
 int main(int argc, char **argv)
 {
 	dprint("Debug mode activated");
+
+	s_init s_i;
+	if (initializer(argc, argv, s_i) != EXIT_SUCCESS)
+	{
+		return (EXIT_FAILURE);
+	}
+	gettimeofday(&s_i.start_time, NULL);
+
+	// First algorithm runs here
+
+	gettimeofday(&s_i.end_time, NULL);
+	get_duration(s_i);
+
+	std::cout << "After " << "\n";
+	std::cout << "Time to process a range of " << s_i.array_size
+			  << " elements with std::[..] : " << s_i.duration << " μs\n";
+
+	gettimeofday(&s_i.start_time, NULL);
+
+	// Second algorithm runs here
+
+	gettimeofday(&s_i.end_time, NULL);
+	get_duration(s_i);
+
+	std::cout << "Time to process a range of " << s_i.array_size
+			  << " elements with std::[..] : " << s_i.duration << " μs\n";
+
+	dprint("");
+	dprint("End of main()");
+	return (EXIT_SUCCESS);
+}
+
+void get_duration(t_init &s_i)
+{
+	long seconds	  = s_i.end_time.tv_sec - s_i.start_time.tv_sec;
+	long microseconds = s_i.end_time.tv_usec - s_i.start_time.tv_usec;
+	s_i.duration	  = seconds * 1000000 + microseconds;
+	dprint("Time duration: " << s_i.duration << " microseconds");
+}
+
+int initializer(int argc, char **argv, t_init &s_i)
+{
+	dprint("Initializing data");
+
 	if (argc < 2)
 	{
 		dprint("No argument provided");
 		std::cerr << "Error\n";
 		return (EXIT_FAILURE);
 	}
-	int maximum	   = atoi(argv[1]);
-	int array_size = 0;
+
+	s_i.maximum_value = atoi(argv[1]);
+	s_i.array_size	  = 0;
+
 	for (int i = 1; argv[i] != 0; ++i)
 	{
-		// dprint("argv[" << i << "]: " << argv[i]);
-		if (0 >= atoi(argv[i]))
+		int current_val = atoi(argv[i]);
+
+		if (0 >= current_val)
 		{
-			dprint("Number elemente equal or smaller than zero");
+			dprint("Number element equal or smaller than zero: " << argv[i]);
 			std::cout << "Error\n";
 			return (EXIT_FAILURE);
 		}
-		if (maximum < atoi(argv[i]))
+		if (s_i.maximum_value < current_val)
 		{
-			maximum = atoi(argv[i]);
+			s_i.maximum_value = current_val;
 		}
-		array_size++;
+		s_i.array_size++;
 	}
-	int nbr_length = get_nbr_length(maximum);
-	dprint("Number length: " << nbr_length << " (" << maximum << ")");
-	dprint("Maximum element: " << maximum
-							   << " length: " << get_nbr_length(maximum));
+
+	s_i.nbr_length = get_nbr_length(s_i.maximum_value);
+	dprint("Maximum element: " << s_i.maximum_value << " (length: "
+							   << get_nbr_length(s_i.maximum_value) << ")");
+
+	gettimeofday(&s_i.start_time, NULL);
+	gettimeofday(&s_i.end_time, NULL);
+	s_i.duration = 0;
+
 	std::cout << "Before ";
 	for (int i = 1; argv[i] != 0; ++i)
 	{
-		if ((80 - 8 - 6) < (i * (nbr_length + 1)))
+		if ((80 - 8 - 6) < (i * (s_i.nbr_length + 1)))
 		{
 			std::cout << " [...]";
 			break;
 		}
-		std::cout << " " << std::setw(nbr_length) << argv[i];
+		std::cout << " " << std::setw(s_i.nbr_length) << argv[i];
 	}
 	std::cout << "\n";
+
+	dprint("Initialization successful");
 	dprint("");
-
-	struct timeval start;
-	struct timeval end;
-	gettimeofday(&start, NULL);
-
-	// Algorithm runs here
-
-	gettimeofday(&end, NULL);
-	long seconds	  = end.tv_sec - start.tv_sec;
-	long microseconds = end.tv_usec - start.tv_usec;
-	long elapsed	  = seconds * 1000000 + microseconds;
-	dprint("Time: " << elapsed << " microseconds");
-
-	std::cout << "After " << "\n";
-	std::cout << "Time to process a range of " << array_size
-			  << " elements with std::[..] : " << elapsed << " μs\n";
-	std::cout << "Time to process a range of " << array_size
-			  << " elements with std::[..] : " << elapsed << " μs\n";
-
-	dprint("");
-	dprint("End of main()");
 	return (EXIT_SUCCESS);
 }
 
@@ -102,10 +145,15 @@ int get_nbr_length(int nbr)
 		len++;
 		nbr = -nbr;
 	}
+	if (nbr == 0)
+	{
+		return 1;
+	}
 	while (nbr > 0)
 	{
 		nbr /= 10;
 		len++;
 	}
+
 	return (len);
 }
