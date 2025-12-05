ce014762edb1e13894eb9e29b563821fa9567beb
delta <185072671+d-branco@users.noreply.github.com>
2025-12-05 18:25:23 +0000

    dacing pairs visualizer

--- 09/ex02/PmergeMe.cpp
+++ 09/ex02/PmergeMe.cpp
@@ -5,7 +5,7 @@
 /*   github.com/d-branco                    +#+         +#+      +#+#+#+      */
 /*                                       +#+         +#+              +#+     */
 /*   Created: 2025/12/03 12:09:12      #+#         #+#      +#+        #+#    */
-/*   Updated: 2025/12/05 15:09:42     #########  #########  ###      ###      */
+/*   Updated: 2025/12/05 18:53:25     #########  #########  ###      ###      */
 /*                                                            ########        */
 /* ************************************************************************** */
 
@@ -13,7 +13,10 @@
 
 void merge_intertion_vec(t_init &s_i)
 {
-	for (unsigned int i = 2; i <= s_i.vicky.size() / 2; i *= 2)
+	dprint("Before pair sorting: ");
+	dprint(print_vec(s_i, 0));
+	dprint("");
+	for (unsigned int i = 1; i <= s_i.vicky.size() / 2; i *= 2)
 	{
 		dprint("Sorting pairs of size: " << i);
 		sort_pairs_vec(s_i, i);
@@ -28,22 +31,44 @@ int sort_pairs_vec(t_init &s_i, unsigned int pair_size)
 	(void) s_i;
 	(void) pair_size;
 
-	// recursion end
-
-	// recursion a priore
-
-	// recall
-
-	// recursion a posteriori
-
+	////
+	for (unsigned int i = 0; i < (s_i.vicky.size() / pair_size);
+		 i += pair_size * 2)
+	{
+		dprint("Swaping " << s_i.vicky[i + pair_size - 1] << " with ...");
+	}
+	////
 	dprint(print_vec(s_i, pair_size));
 	return (0);
 }
 
 std::string print_vec(t_init &s_i, unsigned int pair_size)
 {
-	std::string ret = "Vec: ";
-	for (unsigned int i = 1; i < s_i.vicky.size(); ++i)
+	std::string ret = "SWAP: ";
+
+	if ((pair_size != 0)
+		&& (((pair_size * s_i.nbr_length + (pair_size - 1)) + 1) <= 60))
+	{
+		pair_size *= 2;
+		for (unsigned int i = 0; i < (s_i.vicky.size() / pair_size); ++i)
+		{
+			for (unsigned int j = 1;
+				 j <= (pair_size * s_i.nbr_length + (pair_size - 1));
+				 j++)
+			{
+				ret += "_";
+			}
+			ret += " ";
+			if ((((pair_size * s_i.nbr_length + (pair_size - 1)) + 1) * (i + 1))
+				> 60)
+			{
+				break;
+			}
+		}
+		pair_size /= 2;
+	}
+	ret += "\n==DEBUG== Vec: ";
+	for (unsigned int i = 0; i < s_i.vicky.size(); ++i)
 	{
 		if ((80 - 15 - 6) < (i * (s_i.nbr_length + 1)))
 		{
